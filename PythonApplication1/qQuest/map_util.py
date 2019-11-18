f75e3e0c9e708338da7a7cdff7dc155acac081e7
Jacob Blumoff <jacobblumoff@Jacobs-MacBook-Pro.local>
2019-11-18 07:13:09 -0800

    More refactoring of the room construction tools

--- PythonApplication1/qQuest/map_util.py
+++ PythonApplication1/qQuest/map_util.py
@@ -1,6 +1,6 @@
 import tcod as libtcod
 import pygame
-import json, os
+import json, os, copy
 import numpy as np
 
 #from qQuest import constants
@@ -64,84 +64,105 @@ def mapCalculateFov(actor):
                        algorithm = constants.FOV_ALGO)
     actor.fovCalculate = False
 
-def makeWalledRoom(width, height):
-    mapArray = [["#",] * width,]
-    for i in range(height-2):
-        mapArray += [["#",] + ["_",] * (width-2) + ["#",],]
-    mapArray += [["#",] * width,]
-    return np.array(mapArray, dtype=np.str)
+# def makeWalledRoom(width, height):
+#     mapArray = [["#",] * width,]
+#     for i in range(height-2):
+#         mapArray += [["#",] + ["_",] * (width-2) + ["#",],]
+#     mapArray += [["#",] * width,]
+#     return np.array(mapArray, dtype=np.str)
+class Map:
+    def __init__(self, initWidth=10, initHeight=10):
+        self.mapArray = makeRoom(initWidth, initHeight, symbol="#")
+        self.rooms = [] #Id'd by four number:  upperLeftX, upperLeftY, lowerRightX, lowerRightY
+
+    def plot(self):
+        print('\n')
+        for row in self.mapArray:
+            print(''.join(row))
+        print('\n')
+
+    def expandMapLeft(self, dx):
+        self.expandMap((dx, 0), (dx,0))
+
+    def expandMapRight(self, dx):
+        self.expandMap((dx, 0), (0,0))
+
+    def expandMapUp(self, dy):
+        self.expandMap((0, dy), (0,dy))
+
+    def expandMapDown(self, dy):
+        self.expandMap((0, dy), (0,0))
+
+    # Also offset all existing room datas
+    def expandMap(self, deltaTuple, offsetTuple):
+        ySize, xSize = self.mapArray.shape
+        oldRoom = copy.copy(self.mapArray)
+        self.mapArray = makeRoom(xSize+deltaTuple[0], ySize+deltaTuple[1], symbol="#")
+
+        dx, dy = offsetTuple
+        self.placeRoom(oldRoom, x=dx, y=dy, addRoomIndex=False)
+        for i, roomList in enumerate(self.rooms):
+            [a, b, c, d] = roomList
+            self.rooms[i] = [a+dx, b+dy, c+dx, d+dy]
+        del oldRoom
+
+    
+    def placeRoom(self, roomArray, x=0, y=0, addRoomIndex=True):
+        mapYSize, mapXSize = self.mapArray.shape
+        roomYSize, roomXSize = roomArray.shape
+
+        dx, dy = 0, 0
+        if (x<0):
+            dx = 0-x #changing the index point
+            self.expandMapLeft(dx)
+            x += dx 
+        if (x+roomXSize > self.mapArray.shape[1]):
+            self.expandMapRight(x+roomXSize-mapXSize)
+        if (y<0):
+            dy = 0-y
+            self.expandMapUp(dy)
+            y += dy
+        if (y+roomYSize > self.mapArray.shape[0]):
+            self.expandMapDown(y+roomYSize-mapYSize)
+
+        for i,xRow in enumerate(self.mapArray):
+            for j, el in enumerate(xRow):
+                # i becomes y, j becomes x
+                if j >= x and j<x+roomXSize and i >= y and i < y+roomYSize:
+                    self.mapArray[i][j] = roomArray[i-y][j-x] 
+
+        #Id'd by four number:  upperLeftX, upperLeftY, lowerRightX, lowerRightY
+        if addRoomIndex:
+            self.rooms.append([x,y,x+roomXSize-1, y+roomYSize-1])
+
 
 def makeRoom(width, height, symbol="_"):
     mapArray = [[symbol,] * width,]*height
     return np.array(mapArray, dtype=np.str)
 
-def placeRoom(mapArray, roomArray, x=0, y=0):
-    mapYSize, mapXSize = mapArray.shape
-    roomYSize, roomXSize = roomArray.shape
-
-    dx, dy = 0, 0
-    if (x<0):
-        dx = 0-x #changing the index point
-        mapArray = expandMapLeft(mapArray, dx)
-        mapYSize, mapXSize = mapArray.shape
-        x += dx 
-    if (x+roomXSize > mapXSize):
-        mapArray = expandMapRight(mapArray, x+roomXSize-mapXSize)
-        mapYSize, mapXSize = mapArray.shape
-    if (y<0):
-        dy = 0-y
-        mapArray = expandMapUp(mapArray, dy)
-        mapYSize, mapXSize = mapArray.shape
-        y += dy
-    if (y+roomYSize > mapYSize):
-        mapArray = expandMapDown(mapArray, y+roomYSize-mapYSize)
-        mapYSize, mapXSize = mapArray.shape
-
-    for i,xRow in enumerate(mapArray):
-        for j, el in enumerate(xRow):
-            # i becomes y, j becomes x
-            if j >= x and j<x+roomXSize and i >= y and i < y+roomYSize:
-                mapArray[i][j] = roomArray[i-y][j-x] 
-    return mapArray
-
-
-
-def expandMapLeft(mapArray, dx):
-    ySize, xSize = mapArray.shape
-    print("Expanding left")
-    newMapArray = makeRoom(xSize+dx, ySize, symbol="#")
-    return placeRoom(newMapArray, mapArray, x=dx, y=0)
-
-def expandMapRight(mapArray, dx):
-    ySize, xSize = mapArray.shape
-    print("Expanding right")
-    newMapArray = makeRoom(xSize+dx, ySize, symbol="#")
-    return placeRoom(newMapArray, mapArray, x=0, y=0)
-
-def expandMapUp(mapArray, dy):
-    ySize, xSize = mapArray.shape
-    print("Expanding up")
-    newMapArray = makeRoom(xSize, ySize+dy, symbol="#")
-    return placeRoom(newMapArray, mapArray, x=0, y=dy)
-
-def expandMapDown(mapArray, dy):
-    ySize, xSize = mapArray.shape
-    print("Expanding down")
-    newMapArray = makeRoom(xSize, ySize+dy, symbol="#")
-    return placeRoom(newMapArray, mapArray, x=0, y=0)
 
 if __name__ == "__main__":
-    mapArray = np.array([["#",]*10,]*10, dtype=np.str)
+    newMap = Map(5,5)
 
-    newRoom = makeRoom(3, 3, symbol="_")
-    mapArray = placeRoom(mapArray, newRoom, x=2, y=2)
+    newRoom = makeRoom(3,3)
+    newMap.placeRoom(newRoom, -1, -1)
+    newMap.plot()
+    print(newMap.rooms)
 
-    newRoom = makeRoom(5, 3, symbol="_")
-    mapArray = placeRoom(mapArray, newRoom, x=2, y=7)
-    print(mapArray)
+    newMap.expandMapLeft(1)
+    newMap.plot()
+    print(newMap.rooms)
 
-    #mapArray = expandMapUp(mapArray, 3)
-    #print(mapArray)
+    newMap.expandMapRight(1)
+    newMap.plot()
+    print(newMap.rooms)
+
+    # mapArray = np.array([["#",]*10,]*10, dtype=np.str)
+
+    # newRoom = makeRoom(3, 3, symbol="_")
+    # mapArray = placeRoom(mapArray, newRoom, x=2, y=2)
+
+    # print(mapArray)
     pass
 
     
